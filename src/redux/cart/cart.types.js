a2e5e3cde26894213c5836979ae88257578de0b3
p0ria <p0ria@live.com>
2020-01-09 18:46:10 +0330

    created cart-item componenet and connected our cart dropdown with cartitem

--- src/redux/cart/cart.types.js
+++ src/redux/cart/cart.types.js
@@ -1,5 +1,6 @@
 const CartActionTypes = {
-    TOGGLE_CART_HIDDEN: 'TOGGLE_CART_HIDDEN'
+    TOGGLE_CART_HIDDEN: 'TOGGLE_CART_HIDDEN',
+    Add_ITEM: 'ADD_ITEM'
 };
 
 export default CartActionTypes;
